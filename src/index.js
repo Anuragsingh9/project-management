6bc0f7379ea3eb026c2ed982b893596c4343d0e9
anurag-crownstack <anurag.singh@crownstack.com>
2024-12-03 15:24:29 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const dbMiddleware = require('./middleware/dbMiddleware');
 const userRoutes = require('./routes/users');
+const organizationRoutes = require('./routes/organizations');
+const projectRoutes = require('./routes/projects');
+const taskRoutes = require('./routes/tasks');
 
 
 app.use(cors());
@@ -13,9 +16,17 @@ app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(dbMiddleware);
 
-app.use('/api/user',userRoutes);
+// user routes
+app.use('/api/user', userRoutes);
 
+// organization routes
+app.use('/api/organization', organizationRoutes);
 
+// project routes
+app.use('/api/project', projectRoutes);
+
+// task routes
+app.use('/api/task', taskRoutes);
 
 const port = process.env.PORT || 8000;
 
